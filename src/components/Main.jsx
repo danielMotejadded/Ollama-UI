8f34ec66cbe0c648c1c0fd08ec1a1fdf7f63c256
Daniel Motejadded <danimotejadded@gmail.com>
2026-07-25 23:07:03 +0200

    Add changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,16 +3,7 @@ import ChatMessage from "./ChatMessage";
 import { useEffect, useRef } from "react";
 
 export default function Main({ activeChat, setChats }) {
-  const containerRef = useRef();
-  const length = activeChat.messages?.length;
-  const lastItem = activeChat.messages[length - 1];
-  useEffect(() => {
-    const container = containerRef.current;
 
-    if (container) {
-      container.scrollTop = container.scrollHeight;
-    }
-  }, [lastItem]);
   if (!activeChat) {
     return (
       <div className="flex-1 flex items-center justify-center text-zinc-400">
@@ -26,7 +17,7 @@ export default function Main({ activeChat, setChats }) {
       <div className="flex-1 overflow-y-auto px-6 py-8">
         <div
           className="w-2/3 mx-auto space-y-6 text-white mb-6"
-          ref={containerRef}
+          
         >
           {activeChat.messages.map((msg) => (
             <ChatMessage key={msg.id} message={msg} />
